c1a9acec55b8c786254d775fd57f2cfaf094023d
Ikaro Souza <ikarolp@hotmail.com>
2016-04-04 23:11:26 -0300

    Create Sector class

--- src/br/ufrn/imd/dominio/Sector.java
+++ src/br/ufrn/imd/dominio/Sector.java
@@ -1,5 +1,35 @@
 package br.ufrn.imd.dominio;
 
-public class Sector {
+import java.util.ArrayList;
 
+public class Sector {
+	private int id;
+	private String name;
+	private ArrayList<Employee> employees;
+	private ArrayList<Equipment> equipments;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Employee> getEmployees() {
+		return employees;
+	}
+	public void setEmployees(ArrayList<Employee> employees) {
+		this.employees = employees;
+	}
+	public ArrayList<Equipment> getEquipments() {
+		return equipments;
+	}
+	public void setEquipments(ArrayList<Equipment> equipments) {
+		this.equipments = equipments;
+	}
 }
