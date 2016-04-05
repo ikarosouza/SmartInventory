d0f30c02556cc9bee67d202db1126221794b9f05
Ikaro Souza <ikarolp@hotmail.com>
2016-04-04 23:02:40 -0300

    Create Employee class

--- src/br/ufrn/imd/dominio/Employee.java
+++ src/br/ufrn/imd/dominio/Employee.java
@@ -1,5 +1,42 @@
 package br.ufrn.imd.dominio;
 
-public class Employee {
+import java.util.ArrayList;
 
+public class Employee {
+	private int matricula;
+	private String name;
+	private String role;
+	private ArrayList<Equipment> equipments;
+	private Sector sector;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public ArrayList<Equipment> getEquipments() {
+		return equipments;
+	}
+	public void setEquipments(ArrayList<Equipment> equipments) {
+		this.equipments = equipments;
+	}
+	public Sector getSector() {
+		return sector;
+	}
+	public void setSector(Sector sector) {
+		this.sector = sector;
+	}
 }
